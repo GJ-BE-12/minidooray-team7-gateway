b3f32e0dd08f0e396bf29cd09763f6113e99bdc3
ryujikang <dbwlrkd2000@naver.com>
2025-11-06 13:03:02 +0900

    first init

---
+++ src/main/java/com/nhnacademy/spring_boot_jpa/NhnAcademySpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.spring_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhnAcademySpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhnAcademySpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/spring_boot_jpa/NhnAcademySpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.spring_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhnAcademySpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
